9b515010b35226a985bb6af736805ba494300dce
Battlehart <sarah.stelljes@gmail.com>
2022-09-23 10:02:26 -0400

    added cards.js and altered form.js by removing newContactButton variable and respective event listener. Also turned toggleForm into a exported function and created a new exported function called clearForm()

--- 19-3-form-codejs.js
+++ 19-3-form-codejs.js
@@ -1,31 +1,4 @@
-// import modules
-import { toggleForm, clearForm } from './form';
-import { initdb, postDb } from './database';
-import { fetchCards } from './cards';
-
-// Import CSS files
-import "../css/index.css";
-
-// Import bootstrap stuff
-import { Tooltip, Toast, Popover } from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-// Import pictures
-import Logo from '../images/logo.png';
-import Bear from '../images/bear.png';
-import Dog from '../images/dog.png';
-
-// use pictures on load
-window.addEventListener('load', function(){
-    initdb();
-    fetchCards();
-
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
-});
-
-// Form functionality
+  // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
 let submitBtnToUpdate = false;
